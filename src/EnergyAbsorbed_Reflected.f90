44b51a315afd2e01fdfcce449379ce92e8d7ec77
Matt Mitchell <matt.s.mitchell@okstate.edu>
2017-11-08 15:30:36 -0600

    Ran fprettify (https://github.com/pseewald/fprettify) to cleanup file formatting

--- src/EnergyAbsorbed_Reflected.f90
+++ src/EnergyAbsorbed_Reflected.f90
@@ -1,14 +1,13 @@
-
   MODULE EnergyAbsorbed_Reflected
 
 
   USE Global
   USE EnclosureGeometry
   USE EnergyBundleLocation
-  
-  
+
+
   IMPLICIT NONE
-  
+
 
   CONTAINS
 
@@ -16,15 +15,15 @@
 
 !******************************************************************************
 !
-! PURPOSE:			Checking whether the energy bundle absorbed or reflected 
+! PURPOSE:			Checking whether the energy bundle absorbed or reflected
 !
 !
 !******************************************************************************
     IMPLICIT NONE
     INTEGER		:: I,J, K, IOS, count
 	REAL(Prec2)  :: R_absorbed
-	   
-!   R_absorbed		Random number generated is used to verify whether the 
+
+!   R_absorbed		Random number generated is used to verify whether the
 !                   intercepted energy is absorbed or reflected by comparing
 !                   it with surface absorptance
 
@@ -32,17 +31,17 @@
 !    or not a log file is written.  Also changed format statements to remove commas
 !    so that RTVT could actually read the file.
 !
-    R_absorbed = Rand(6)     
-!	   
+    R_absorbed = Rand(6)
+!
 IF (SpIndex .eq. 1) then    !Specular energy
      IF(R_absorbed < SpecReflec(SInter))Then
         NAEnergyS(SIndexR,SInter) = NAEnergyS(SIndexR,SInter) + 1   !RS: Total number of energy bundles absorbed
         TSpecA(SIndexR) = TSpecA(SIndexR) + 1    !RS: Total Number of energy bundles absorbed by surface
-        
+
 !   count the number of energy bundles absorbed and emitted
         IF (NCount .NE. 1) THEN !RS: Non-reflected rays
            NAEnergyWR(SIndexR,SInter)=NAEnergyWR(SindexR,Sinter)+1  !RS: Total Number of energy bundles absorbed without reflection
-            
+
 		  if (WriteLogFile) then
             Write(4,101, ADVANCE='YES')'P',SIndex,XLS(SIndex),YLS(SIndex),ZLS(SIndex),SInter,XP(SIndex,SInter),YP(SIndex,SInter),ZP(SIndex,SInter)
           end if
@@ -55,58 +54,58 @@ IF (SpIndex .eq. 1) then    !Specular energy
          end if
 
 111      Format(1(' ',I2,3(' ',f6.3)),' ' '0')
-         
+
          NCount=0   !RS: Debugging: Resetting the counter
-         
+
          TSpecR(SIndexR)=TSpecR(SIndexR)+1  !RS: Total number of reflected or reflected rays absorbed by surface
 
          NAEnergyR(SIndexR,SInter)=NAEnergyR(SIndexR,SInter)+1  !RS: Total number of reflected or rereflected rays absorbed
-		 
-        END IF		
+
+        END IF
 
          NTrials = NTrials + 1  !RS: Overall number of rays absorbed from the emitting surface
-		   
+
         IF (SIndex ==1 .and. SInter == 2)Then
-           count = count + 1	 
+           count = count + 1
 
-        ENDIf         
+        ENDIf
 
-	    SIndex = SIndexR 
+	    SIndex = SIndexR
 	    REF_IND = 0
 	    Reflected = .False.   !RS: Setting the reflection flag to false since the array has now been absorbed
         TCountSpecR = 0 !RS:Resetting Reflection Flag
-		          		     		
-     ELSE   
+
+     ELSE
         IF(SIndex == SIndexR .and. REF_IND == 0)Then    !RS: Reflected Rays
-            
+
          NCount=1   !RS: Marking a reflection
-                  
+
          TCountSpecR=1  !RS: Setting a flag
 
         if (WriteLogFile) then
            Write(4,112, ADVANCE='NO')'P',SIndex,XLS(SIndex),YLS(SIndex),ZLS(SIndex),SInter,XP(SIndex,SInter),YP(SIndex,SInter),ZP(SIndex,SInter)
         end if
-            
+
 112     Format(A1,2(' ',I2,3(' ',f6.3)))
 
         ELSEIF(REF_IND == 1)Then    !RS: Rereflected rays
-         
+
          TCountSpecR=2  !RS: Debugging: Setting a flag
-         
+
            if (WriteLogFile) then
             Write(4,102,ADVANCE='NO')SInter, XP(SIndex,SInter),YP(SIndex,SInter),ZP(SIndex,SInter)
            end if
 
 102     format(' ',1(I2,3(' ',f6.3)))
-        
+
         END IF
-        
+
         OldSurface=SIndex  !RS: Keeping track of the emitting surface for each bounce
         SIndex=SInter
 		 Reflected= .True.  !RS: Setting a flag for reflection
          REF_IND=1  !RS: A flag to say the ray has already been reflected at least once
-         
-     ENDIF	
+
+     ENDIF
 Else    !Diffuse Energy
     IF(R_absorbed < Emit(SInter))Then
         NAEnergy(SIndexR,SInter) = NAEnergy(SIndexR,SInter) + 1
@@ -124,48 +123,43 @@ Else    !Diffuse Energy
 		  end if
 
 		 NCountd=0  !RS: Resetting the reflection counter
-         
-        END IF		
 
-         NTrialsd = NTrialsd + 1  
-        
+        END IF
+
+         NTrialsd = NTrialsd + 1
+
         IF (SIndex ==1 .and. SInter == 2)Then
-           count = count + 1	 
-        ENDIf         
+           count = count + 1
+        ENDIf
 
-	    SIndex = SIndexR 
+	    SIndex = SIndexR
 	    REF_IND = 0
 	    Reflected = .False.  !RS: Setting the reflection flag to false since the array has now been absorbed
-		          		     		
-     ELSE   
+
+     ELSE
         IF(SIndex == SIndexR .and. REF_IND == 0)Then
-         TCOUNTR(SIndexR) = TCOUNTR(SIndexR) + 1  
+         TCOUNTR(SIndexR) = TCOUNTR(SIndexR) + 1
 
          NCountd = 1    !RS: Counting as a reflected surface
-         
+
             if (WriteLogFile) then
                 Write(4,112, ADVANCE='NO')'P',SIndex,XLS(SIndex),YLS(SIndex),ZLS(SIndex),SInter,XP(SIndex,SInter),YP(SIndex,SInter),ZP(SIndex,SInter)
             end if
         ELSEIF(REF_IND == 1)Then
 	     TCOUNTRR(SIndexR)= TCOUNTRR(SIndexR) + 1
-         
+
            if (WriteLogFile) then
           Write(4,102,ADVANCE='NO')SInter, XP(SIndex,SInter),YP(SIndex,SInter),ZP(SIndex,SInter)
 			end if
 
         END IF
-        
+
         SIndex=SInter
 		 Reflected= .True.
-         REF_IND=1 
-         
-     ENDIF	
+         REF_IND=1
+
+     ENDIF
 Endif
 
   END SUBROUTINE AbsorptionReflection
   END MODULE EnergyAbsorbed_Reflected
-
-
-
-
-			 	
--- src/Main_MonteCarlo.f90
+++ src/Main_MonteCarlo.f90
@@ -1,7 +1,7 @@
 PROGRAM Main_MonteCarlo
-    !Program and Modules created by Bereket Nigusse, Fall 2004 for MAE 5823
-    !Program and Modules updated and modified November 2012 by
-    !John Holman, Rachel Spitler, and Sudha Sikha for MAE 5823
+   !Program and Modules created by Bereket Nigusse, Fall 2004 for MAE 5823
+   !Program and Modules updated and modified November 2012 by
+   !John Holman, Rachel Spitler, and Sudha Sikha for MAE 5823
 
    USE Global
    USE EnclosureGeometry
@@ -13,7 +13,7 @@ PROGRAM Main_MonteCarlo
    USE OutPut
 
    IMPLICIT NONE
-   INTEGER		 ::I,J,k,IOS,Index, logfileint
+   INTEGER                 ::I, J, k, IOS, Index, logfileint
 !
 !  Initialize the CPU time
    CALL CPU_TIME(TIME1)
@@ -28,141 +28,138 @@ PROGRAM Main_MonteCarlo
 !                                      For output:            MCoutput.txt
 !                                      For temperatures in K: input.TK
 !                                      For number of bundles: parameters.txt
-    Open (Unit=2, file='input.vs3',status='unknown',Action='Read',IOSTAT=IOS)
-	Open (Unit=3, file='MCoutput.txt',status='unknown',IOSTAT = IOS)    !Diffuse bundles and distribution factors
- 	Open (Unit=4, file='logfile.out',status='unknown',IOSTAT = IOS)     !Ray emission, reflection, and absorption points
- 	Open (Unit=7, File='input.TK',status='unknown',IOSTAT = IOS)        !Surface temperatures
-    OPEN (Unit=6, File='SpecularDF.out',status='unknown', IOSTAT=IOS)   !Total Specular bundles and distribution factors
-    OPEN (Unit=9, File='SpecReflecDF.out',status='unknown', IOSTAT=IOS) !Reflected and rereflected Specular bundles and distribution factors
-    OPEN (Unit=10, File='SpecWRDF.out',status='unknown', IOSTAT=IOS)    !Non-Reflected AKA absorbed on first intersection Specular bundles and distribution factors
-    OPEN (Unit=11, File='DebugFile.txt',status='unknown', IOSTAT=IOS)   !Lists rays that are not finding intersection points and whether they're reflected or not
+   Open (Unit=2, file='input.vs3', status='unknown', Action='Read', IOSTAT=IOS)
+   Open (Unit=3, file='MCoutput.txt', status='unknown', IOSTAT=IOS) !Diffuse bundles and distribution factors
+   Open (Unit=4, file='logfile.out', status='unknown', IOSTAT=IOS) !Ray emission, reflection, and absorption points
+   Open (Unit=7, File='input.TK', status='unknown', IOSTAT=IOS) !Surface temperatures
+   OPEN (Unit=6, File='SpecularDF.out', status='unknown', IOSTAT=IOS) !Total Specular bundles and distribution factors
+   OPEN (Unit=9, File='SpecReflecDF.out', status='unknown', IOSTAT=IOS) !Reflected and rereflected Specular bundles and distribution factors
+   OPEN (Unit=10, File='SpecWRDF.out', status='unknown', IOSTAT=IOS) !Non-Reflected AKA absorbed on first intersection Specular bundles and distribution factors
+   OPEN (Unit=11, File='DebugFile.txt', status='unknown', IOSTAT=IOS) !Lists rays that are not finding intersection points and whether they're reflected or not
 
 !   JDS 11-9-2006 Read Nbundles from file so that it can be changed
 !   without recompiling.
 !   JDS 11-20-2006 Read flag to determine whether or not logfile should
 !   be written for use with RTVT
 
- 	Open (Unit=8, File='parameters.txt',status='old',IOSTAT = IOS)
- 	Read (8,*)NBundles
-	read (8,*) logfileint
-	if (logfileint == 1) then
-	   WriteLogFile=.true.
-	   else
-	   WriteLogFile=.false.
-	   end if
-	Close(Unit = 8)
+   Open (Unit=8, File='parameters.txt', status='old', IOSTAT=IOS)
+   Read (8, *) NBundles
+   read (8, *) logfileint
+   if (logfileint == 1) then
+      WriteLogFile = .true.
+   else
+      WriteLogFile = .false.
+   end if
+   Close (Unit=8)
 !
 !
-    Call CalculateGeometry()
-    Call InitializeSeed()
-    Call AllocateArrays()
-	Call InitializeArrays()
+   Call CalculateGeometry()
+   Call InitializeSeed()
+   Call AllocateArrays()
+   Call InitializeArrays()
 !
-    Do SIndex = 1, NSurf
+   Do SIndex = 1, NSurf
       CALL Calculate_SurfaceEquation()
-	  CALL Calculate_Area_Surfaces()
+      CALL Calculate_Area_Surfaces()
       CALL TangentVectors()
-    END DO
+   END DO
 !
 !  Initialize the logical variable for the first emitted energy bundle
    Reflected = .False.
 !
-    DO SIndexR = 1, NSurf
-      SIndex  = SIndexR
+   DO SIndexR = 1, NSurf
+      SIndex = SIndexR
 !
 !  The counter only counts the emitted and absorbed energy
-              Ntrials = 0
- If (SurfaceType(SIndex) .EQ. 'SDE') then
+      Ntrials = 0
+      If (SurfaceType(SIndex) .EQ. 'SDE') then
 
-    SpIndex = 1 !JH: This begins the Specular ray tracing
+         SpIndex = 1 !JH: This begins the Specular ray tracing
 
- Specular:    DO
+         Specular: DO
 !  Calculating source locations for each energy bundle
-	         Call EnergySourceLocation()
+            Call EnergySourceLocation()
 !
 !  Calculate the direction of the emitted energy bundle
-		     CALL DirectionEmittedEnergy()
+            CALL DirectionEmittedEnergy()
 !
 !  Check the intersection points and determine the correct one
-    		 CALL CheckingIntersection()
+            CALL CheckingIntersection()
 
 !  Determine whether the energy bundle is absorbed or reflected
-		     CALL AbsorptionReflection()
+            CALL AbsorptionReflection()
 
 !  If the number of absorbed energy bundles is the same as the number
 !  of emitted energy bundles, exit the specular emission loop
 !
-			 If(NTrials==NBundles) Exit Specular
- END DO Specular
+            If (NTrials == NBundles) Exit Specular
+         END DO Specular
 
- SpIndex = 0 !JH: This begins the diffuse ray tracing for SDE surfaces
+         SpIndex = 0 !JH: This begins the diffuse ray tracing for SDE surfaces
 
-Ntrialsd=0
- Diffuse1:    DO
+         Ntrialsd = 0
+         Diffuse1: DO
 !  Calculating source locations for each energy bundle
-	         Call EnergySourceLocation()
+            Call EnergySourceLocation()
 !
 !  Calculate the direction of emitted energy bundle
-		     CALL DirectionEmittedEnergy()
+            CALL DirectionEmittedEnergy()
 !
 !  Check the intersection points and determine the correct one
-    		 CALL CheckingIntersection()
-
-!  Detrmine whether the enrgy bundle is absorbed or reflected
-		     CALL AbsorptionReflection()
-
-!  If the number of absorbed energy bundles is the same as the
-!  number of emitted energy bundles, exit the diffuse emission loop
-!
-			 If(NTrialsd==NBundles) Exit Diffuse1
- END DO Diffuse1
-
- ElseIF (SurfaceType(SIndex) .EQ. "DIF" .OR. SurfaceType(SIndex) .EQ. "SDR") THEN
-     NTrialsd=0
-    !JH: This is the standard diffuse ray tracing
-     Diffuse2:    DO
-
-!  Calculating source locations for each energy bundle
-	         Call EnergySourceLocation()
-!
-!  Calculate the direction of emitted energy bundle
-		     CALL DirectionEmittedEnergy()
-!
-!  Check the intersection points and determine the correct one
-    		 CALL CheckingIntersection()
+            CALL CheckingIntersection()
 
 !  Determine whether the energy bundle is absorbed or reflected
-		     CALL AbsorptionReflection()
+            CALL AbsorptionReflection()
 
 !  If the number of absorbed energy bundles is the same as the
 !  number of emitted energy bundles, exit the diffuse emission loop
 !
-			 If(NTrialsd==NBundles) Exit Diffuse2
-     END DO Diffuse2
- End if
-    END DO
+            If (NTrialsd == NBundles) Exit Diffuse1
+         END DO Diffuse1
+
+      ElseIF (SurfaceType(SIndex) .EQ. "DIF" .OR. SurfaceType(SIndex) .EQ. "SDR") THEN
+         NTrialsd = 0
+         !JH: This is the standard diffuse ray tracing
+         Diffuse2: DO
+
+!  Calculating source locations for each energy bundle
+            Call EnergySourceLocation()
+!
+!  Calculate the direction of emitted energy bundle
+            CALL DirectionEmittedEnergy()
+!
+!  Check the intersection points and determine the correct one
+            CALL CheckingIntersection()
+
+!  Determine whether the energy bundle is absorbed or reflected
+            CALL AbsorptionReflection()
+
+!  If the number of absorbed energy bundles is the same as the
+!  number of emitted energy bundles, exit the diffuse emission loop
+!
+            If (NTrialsd == NBundles) Exit Diffuse2
+         END DO Diffuse2
+      End if
+   END DO
 !
 !  Calculate the radiation distribution factor
-    Call Rad_Distribution_Factors()
+   Call Rad_Distribution_Factors()
 !
 !  Calculate the heat balance of the enclosure
-	Call Radiation_Balance
+   Call Radiation_Balance
 !
 !  Calculate the CPU Time
-	CALL CPU_TIME(TIME2)
+   CALL CPU_TIME(TIME2)
 
 !  Write Results to a file
-	Call Print_ViewFactor_HeatFlux
+   Call Print_ViewFactor_HeatFlux
 
 !
-     Close(Unit = 3)
-	 Close(Unit = 4)
-	 Close(Unit = 7)
-     CLOSE(Unit = 6)
-     CLOSE(Unit =10)
-     CLOSE(Unit =11)
-	 STOP
+   Close (Unit=3)
+   Close (Unit=4)
+   Close (Unit=7)
+   CLOSE (Unit=6)
+   CLOSE (Unit=10)
+   CLOSE (Unit=11)
+   STOP
 END PROGRAM Main_MonteCarlo
-
-
-
